d155e1f4cfb3776e3cdebf98c3d9e5ba3d30373f
Raju14321 <100340131+Raju14321@users.noreply.github.com>
2022-03-08 18:59:41 +0530

    Add files via upload

---
+++ Finalizer/Finalizer/DestDemo2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finalizer
+{
+    class DestDemo2 : DestDemo1
+    {
+        public DestDemo2()
+        {
+            Console.WriteLine("Instance2 is created.");
+        }
+        ~DestDemo2()
+        {
+            Console.WriteLine("Instance2 is destroyed.");
+        }
+        static void Main()
+        {
+            DestDemo2 obj = new DestDemo2();
+            obj = null;
+            GC.Collect();
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ Finalizer/Finalizer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finalizer
+{
+    class DestDemo1
+    {
+      public DestDemo1()
+      {
+            Console.WriteLine("Instance off created");
+      }
+      ~DestDemo1()
+      {
+            Console.WriteLine("Instance of destroye");
+      }
+       static void Main()
+       {
+            DestDemo1 d1 = new DestDemo1();
+            DestDemo1 d2 = new DestDemo1();
+            DestDemo1 d3 = new DestDemo1();
+            DestDemo1 d4 = new DestDemo1();
+            d2 = null;
+            GC.Collect();
+            Console.ReadLine();
+            
+            
+       }
+
+    }
+}
